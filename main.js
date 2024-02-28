b9017e977d08f849703a9c8502d85360bf10c534
Shourov Soron <shourovhossainsoron@gmail.com>
2024-02-28 12:00:55 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let MenuIcon=document.getElementById("mobile-menu-icon");
+let MobileMenu=document.getElementById("mobile-menu");
+
+MenuIcon.addEventListener("click", () =>{
+    MobileMenu.classList.toggle('hidden');
+
+})
